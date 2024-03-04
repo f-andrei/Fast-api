ef198d6b3042fb1539cbb00c823797050db7e10c
f-andrei <andreifrancap@gmail.com>
2024-03-04 13:38:39 -0300

    add: repeat days post and get

--- app/repos/task_crud.py
+++ app/repos/task_crud.py
@@ -1,6 +1,8 @@
+from typing import List
 from sqlalchemy.orm import Session
-from app.models.database_init import Task
+from app.models.database_init import Task, RepeatDays
 from app.schemas.validation import Task as PydanticTask
+from app.schemas.validation import RepeatDays as PydanticRepeatDays
 
 
 def create_task(session: Session, task_data: PydanticTask): # type: ignore
@@ -39,3 +41,21 @@ def delete_task(session: Session, task_id: int): # type: ignore
         return True
     return False
 
+def add_repeat_days(session: Session, task_id: int, repeat_days: List[int]):
+    try:
+        task = session.query(Task).filter(Task.id == task_id).first()
+        if not task:
+            return False
+        for day_number in repeat_days:
+            repeat_day = RepeatDays(task_id=task_id, day_number=day_number)
+            session.add(repeat_day)
+
+        session.commit()
+        return True
+    except Exception as e:
+        print(e)
+        return False
+    
+
+def get_repeat_days(session: Session, task_id: int):
+    return session.query(RepeatDays).filter(RepeatDays.task_id == task_id).all()
