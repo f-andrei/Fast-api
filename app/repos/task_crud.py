9f20a4cd7a21e4d1e0f21f0eaaf7fa51b1a8e7c3
Andrei <andreifrancap@gmail.com>
2024-03-10 12:47:26 -0300

    fix: code readability, get due tasks endpoint

--- app/repos/task_crud.py
+++ app/repos/task_crud.py
@@ -2,10 +2,9 @@ from typing import List
 from sqlalchemy.orm import Session
 from app.models.database_init import Task, RepeatDays
 from app.schemas.validation import Task as PydanticTask
-from app.schemas.validation import RepeatDays as PydanticRepeatDays
 
 
-def create_task(session: Session, task_data: PydanticTask): # type: ignore
+def create_task(task_data: PydanticTask, session: Session): # type: ignore
     validated_data = task_data.model_dump()
 
     new_task = Task(**validated_data)
@@ -16,13 +15,16 @@ def create_task(session: Session, task_data: PydanticTask): # type: ignore
 
     return new_task
 
-def get_task(session: Session, task_id: int): # type: ignore
+
+def get_task(task_id: int, session: Session): # type: ignore
     return session.query(Task).filter(Task.id == task_id).first()
 
-def get_all_tasks(session: Session, user_id: str): # type: ignore
+
+def get_all_tasks(user_id: str, session: Session): # type: ignore
     return session.query(Task).filter(Task.user_id == user_id).all()
 
-def update_task(session: Session, task_id: int, task_data: PydanticTask): # type: ignore
+
+def update_task(task_data: PydanticTask, task_id: int, session: Session): # type: ignore
     task = session.query(Task).filter(Task.id == task_id).first()
     if task:
         # Update only the provided fields
@@ -33,7 +35,8 @@ def update_task(session: Session, task_id: int, task_data: PydanticTask): # type
         return task
     return None
 
-def delete_task(session: Session, task_id: int): # type: ignore
+
+def delete_task(task_id: int, session: Session): # type: ignore
     task = session.query(Task).filter(Task.id == task_id).first()
     if task:
         session.delete(task)
@@ -41,7 +44,8 @@ def delete_task(session: Session, task_id: int): # type: ignore
         return True
     return False
 
-def add_repeat_days(session: Session, task_id: int, repeat_days: List[int]):
+
+def add_repeat_days(repeat_days: List[int], task_id: int, session: Session):
     try:
         task = session.query(Task).filter(Task.id == task_id).first()
         if not task:
@@ -57,5 +61,10 @@ def add_repeat_days(session: Session, task_id: int, repeat_days: List[int]):
         return False
     
 
-def get_repeat_days(session: Session, task_id: int):
+def get_repeat_days(task_id: int, session: Session):
     return session.query(RepeatDays).filter(RepeatDays.task_id == task_id).all()
+
+
+def get_due_tasks(user_id: str, session: Session):
+    return session.query(Task).filter(user_id).all()
+
--- app/routers/task_routes.py
+++ app/routers/task_routes.py
@@ -2,10 +2,14 @@ from typing import List
 from fastapi import HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session as DBSession
 from app.models.database_init import Session as TaskSession
-from app.schemas.validation import Task, RepeatDays
-from app.repos.task_crud import create_task, get_all_tasks, get_task, update_task, delete_task, add_repeat_days, get_repeat_days
+from app.schemas.validation import Task
+from app.repos.task_crud import (
+    create_task, get_all_tasks, get_task, update_task, 
+    delete_task, add_repeat_days, get_repeat_days, get_due_tasks)
+
 
 task_router = APIRouter()
+
 
 def get_db():
     db = TaskSession()
@@ -14,10 +18,11 @@ def get_db():
     finally:
         db.close()
 
+
 @task_router.post("/task/create_task")
-def create_task_endpoint(task_data: Task, db: DBSession = Depends(get_db)):
+def create_task_endpoint(task_data: Task, db: DBSession=Depends(get_db)):
     try:
-        return create_task(session=db, task_data=task_data)
+        return create_task(task_data=task_data, session=db)
     except Exception as e:
         print(e)
 
@@ -26,43 +31,41 @@ def create_task_endpoint(task_data: Task, db: DBSession = Depends(get_db)):
 
 
 @task_router.get("/task/get_task/{task_id}")
-def get_task_endpoint(task_id: int, db: DBSession = Depends(get_db)):
+def get_task_endpoint(task_id: int, db: DBSession=Depends(get_db)):
     try:
-        return get_task(session=db, task_id=task_id)
+        return get_task(task_id=task_id, session=db)
     except Exception as e:
         print(e)
         raise HTTPException(status_code=500, detail="Internal Server Error")
     
 
 @task_router.get("/task/get_all_tasks/{user_id}")
-def get_task_endpoint(user_id: str, db: DBSession = Depends(get_db)):
+def get_task_endpoint(user_id: str, db: DBSession=Depends(get_db)):
     try:
-        return get_all_tasks(session=db, user_id=user_id)
+        return get_all_tasks(user_id=user_id, session=db)
     except Exception as e:
         print(e)
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
 
 @task_router.put("/task/update_task/{task_id}")
-def update_task_endpoint(task_id: int, task_data: dict, db: DBSession = Depends(get_db)):
+def update_task_endpoint(task_data: dict, task_id: int, db: DBSession=Depends(get_db)):
     try:
-        updated_task = update_task(session=db, task_id=task_id, task_data=task_data)
+        updated_task = update_task(task_data=task_data, task_id=task_id, session=db)
         if updated_task:
             return {"message": "Task updated successfully", "updated_task": updated_task}
         else:
             raise HTTPException(status_code=404, detail="Task not found")
     except Exception as e:
         print(e)
-
         db.rollback()
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
-    
 
 @task_router.delete("/task/delete_task/{task_id}")
-def delete_task_endpoint(task_id: int, db: DBSession = Depends(get_db)):
+def delete_task_endpoint(task_id: int, db: DBSession=Depends(get_db)):
     try:
-        return delete_task(session=db, task_id=task_id)
+        return delete_task(task_id=task_id, session=db)
     except Exception as e:
         print(e)
         db.rollback()
@@ -70,19 +73,29 @@ def delete_task_endpoint(task_id: int, db: DBSession = Depends(get_db)):
     
 
 @task_router.post("/task/add_repeat_days/{task_id}")
-def add_repeat_days_endpoint(repeat_days: List[int], task_id: int, db: DBSession = Depends(get_db)):
+def add_repeat_days_endpoint(repeat_days: List[int], task_id: int, db: DBSession=Depends(get_db)):
     try: 
-        return add_repeat_days(session=db, task_id=task_id, repeat_days=repeat_days)
+        return add_repeat_days(repeat_days=repeat_days, task_id=task_id, session=db)
     except Exception as e:
         print(e)
         db.rollback()
         raise HTTPException(status_code=500, detail="Internal Server Error")
     
+
 @task_router.get("/task/get_repeat_days/{task_id}")
-def get_repeat_days_endpoint(task_id: int, db: DBSession = Depends(get_db)):
+def get_repeat_days_endpoint(task_id: int, db: DBSession=Depends(get_db)):
     try:
-        return get_repeat_days(session=db, task_id=task_id)
+        return get_repeat_days(task_id=task_id, session=db)
     except Exception as e:
         print(e)
         raise HTTPException(status_code=500, detail="Internal Server Error")
         
+
+@task_router.get("/task/get_due_tasks/{user_id}")
+def get_due_tasks_endpoint(user_id: str, db: DBSession=Depends(get_db)):
+    try:
+        return get_due_tasks(user_id=user_id, session=db)
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+    
