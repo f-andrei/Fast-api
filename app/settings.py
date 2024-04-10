71707eaaeb7f2523d7e45d23611f6accf92f6ed4
Andrei <andreifrancap@gmail.com>
2024-04-10 16:56:34 -0300

    fix: added db envs to test locally

--- app/settings.py
+++ app/settings.py
@@ -1,8 +1,19 @@
 import os
 from datetime import datetime, timedelta
 from pytz import timezone
+from dotenv import load_dotenv
 
-DATABASE_URL = os.getenv('DATABASE_URL').replace("://", "ql://", 1)
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+#DATABASE_URL = os.getenv('DATABASE_URL').replace("://", "ql://", 1)
 
 def time_range():
     tz = "America/Sao_Paulo"
