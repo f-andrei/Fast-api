e5e4dbfa0843897b4e4fb6e6eef76f6a0ffb8f4c
f-andrei <andreifrancap@gmail.com>
2024-04-11 16:51:33 -0300

    fix: changed db url

--- app/settings.py
+++ app/settings.py
@@ -7,14 +7,15 @@ load_dotenv()
 
 API_URL = os.getenv("API_URL")
 
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
+DATABASE_URL = os.getenv('DATABASE_URL').replace("://", "ql://", 1)
 
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-#DATABASE_URL = os.getenv('DATABASE_URL').replace("://", "ql://", 1)
+# Use the commented code below to run locally using pgadmin4
+#DB_USER = os.getenv("DB_USER")
+#DB_PASSWORD = os.getenv("DB_PASSWORD")
+#DB_HOST = os.getenv("DB_HOST")
+#DB_PORT = os.getenv("DB_PORT")
+#DB_NAME = os.getenv("DB_NAME")
+#DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 def time_range():
     tz = "America/Sao_Paulo"
